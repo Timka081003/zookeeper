32c9cf6d5e47ff15b98f31d21939d44c673cfe08
Timka081003 <77959253+Timka081003@users.noreply.github.com>
2021-01-30 20:58:54 +0600

    topics arrays completed

--- zookeeper.py
+++ zookeeper.py
@@ -1,4 +1,12 @@
-print("Did that stop the old Grinch?\n"
-       "No! The Grinch simply said,\n"
-       "\"If I can't find a reindeer,\n"
-       "I'll make one instead!\"")
+# Save the input in this variable
+ticket = input()
+
+# Add up the digits for each half
+half1 = (int(ticket[0]) + int(ticket[1])+ int(ticket[2]))
+half2 = (int(ticket[3]) + int(ticket[4])+ int(ticket[5]))
+
+# Thanks to you, this code will work
+if half1 == half2:
+    print("Lucky")
+else:
+    print("Ordinary")
